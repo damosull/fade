f3055e62b0d39ac8b5b4f9fcb156bd00e1f75245
damosull <damienosullivan3266@gmail.com>
2025-11-26 16:20:11 +0000

    lifetime isa

--- tests/financesTab.spec.ts
+++ tests/financesTab.spec.ts
@@ -164,4 +164,35 @@ test.describe('Finances Tab', () => {
     await expect(assetRow).toContainText(newPolicy);
     await expect(assetRow).toContainText('in force');
   });
+
+  test('Verify adding Lifetime ISA', async ({ app }) => {
+    const ownerTag = `e2e-${stamp()}`;
+    const isaPolicyName = `ISA-${ownerTag}`;
+    const seed = getSeed();
+    const newPolicy = policyNumber();
+    const provider = 'Aviva';
+
+    await app.actions.header.searchForAccount(seed.accountSurname);
+    await app.actions.header.openFinancesTab();
+    await app.pages.finance.addAssetButton().click();
+    await app.actions.assetModal.addAssetOverview('isa');
+    await app.actions.assetModal.addAssetSubType('Lifetime ISA');
+
+    await app.actions.assetModal.addAssetPolicyDetails(
+      isaPolicyName,
+      provider,
+      newPolicy,
+      'in force'
+    );
+
+    await app.actions.assetModal.addPolicyStatusDate();
+    await app.actions.assetModal.saveAsset();
+
+    const assetRow = app.pages.finance.assetRowByName(isaPolicyName);
+    await expect(assetRow).toContainText(provider);
+    await expect(assetRow).toContainText('ISA');
+    await expect(assetRow).toContainText('lifetime ISA');
+    await expect(assetRow).toContainText(newPolicy);
+    await expect(assetRow).toContainText('in force');
+  });
 });
