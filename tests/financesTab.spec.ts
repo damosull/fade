0b2dac90142f914decd936031b99a56d85b5a91a
damosull <damienosullivan3266@gmail.com>
2025-11-26 17:03:22 +0000

    Finances Tab - Verify adding Pension - Workplace

--- tests/financesTab.spec.ts
+++ tests/financesTab.spec.ts
@@ -228,4 +228,37 @@ test.describe('Finances Tab', () => {
     await expect(assetRow).toContainText('in force');
     await expect(assetRow).toContainText('under agency');
   });
+
+  test('Verify adding Pension - Workplace', async ({ app }) => {
+    const ownerTag = `e2e-${stamp()}`;
+    const isaPolicyName = `ISA-${ownerTag}`;
+    const seed = getSeed();
+    const newPolicy = policyNumber();
+    const provider = 'Aviva';
+
+    await app.actions.header.searchForAccount(seed.accountSurname);
+    await app.actions.header.openFinancesTab();
+    await app.pages.finance.addAssetButton().click();
+    await app.actions.assetModal.addAssetOverview('pension');
+    await app.actions.assetModal.addAssetSubType('Workplace');
+    await app.actions.assetModal.addPensionType('AVC');
+    await app.actions.assetModal.addAssetAgencyStatus('under agency');
+
+    await app.actions.assetModal.addAssetPolicyDetails(
+      isaPolicyName,
+      provider,
+      newPolicy,
+      'in force'
+    );
+
+    await app.actions.assetModal.saveAsset();
+
+    const assetRow = app.pages.finance.assetRowByName(isaPolicyName);
+    await expect(assetRow).toContainText(provider);
+    await expect(assetRow).toContainText('pension');
+    await expect(assetRow).toContainText('workplace');
+    await expect(assetRow).toContainText(newPolicy);
+    await expect(assetRow).toContainText('in force');
+    await expect(assetRow).toContainText('under agency');
+  });
 });
